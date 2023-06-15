f5e28ef5932046489df48bf01f8ab74e9a36d8ad
MARIA ZURITA <77291631+MZURITT@users.noreply.github.com>
2023-06-16 01:24:32 +0200

    logs terminado

--- src/connexion/Conexionbd.java
+++ src/connexion/Conexionbd.java
@@ -1,7 +1,14 @@
 package connexion;
 
+import java.io.IOException;
 import java.sql.*;
 import java.util.Scanner;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+import static log.Logs.LOGGER;
 
 public class Conexionbd {
 
@@ -10,6 +17,21 @@ public class Conexionbd {
      */
     static Scanner sc = new Scanner(System.in);
 
+    // Logger para el registro de consultas
+    private static final Logger LOGGER = Logger.getLogger(Conexionbd.class.getName());
+    private static FileHandler fileHandler;
+
+    static {
+        try {
+            fileHandler = new FileHandler("consulta.log", true);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fileHandler.setFormatter(formatter);
+            LOGGER.setUseParentHandlers(false);
+            LOGGER.addHandler(fileHandler);
+        } catch (IOException e) {
+            LOGGER.severe("Error al inicializar el archivo de registro: " + e.getMessage());
+        }
+    }
     /**
      * Conexión con la base de datos.
      */
@@ -38,7 +60,9 @@ public class Conexionbd {
             /**
              * Si no se ha producido ningún error, se muestra un mensaje indicando que la
              */
-            System.out.println("Conexión establecida con la base de datos ");
+
+            // Registro de la consulta de conexión
+            LOGGER.info("Conexión establecida con la base de datos");
         } catch (SQLException e) {
             /**
              * Se muestra un mensaje de error indicando que no se ha podido establecer la
@@ -55,6 +79,8 @@ public class Conexionbd {
     public static void desconectar() {
         try {
             conexion.close();
+        // Registro de la consulta de desconexión
+        LOGGER.info("Conexión cerrada con la base de datos");
         } catch (SQLException e) {
             System.out.println("Error al desconectar con la base de datos");
             e.printStackTrace();
@@ -75,6 +101,9 @@ public class Conexionbd {
             String sql = "SELECT * FROM dueño WHERE telefono='" + telefono + "'";
             ResultSet rs = st.executeQuery(sql);
 
+            // Registro de la consulta de búsqueda
+            LOGGER.info("Consulta de búsqueda de dueño por teléfono");
+
             while (rs.next()) {
                 System.out.println("Nombre: " + rs.getString("nombre") + " Telefono: " + rs.getInt("telefono"));
             }
@@ -82,6 +111,9 @@ public class Conexionbd {
         } catch (SQLException e) {
             System.out.println("Error al buscar dueño");
             e.printStackTrace();
+
+            // Registro del error en la consulta de búsqueda de dueño
+            LOGGER.log(Level.SEVERE, "Error al buscar dueño", e);
         }
     }
 
@@ -100,6 +132,9 @@ public class Conexionbd {
             String sql = "SELECT * FROM empleado WHERE telefono='" + telefono + "'";
             ResultSet rs = st.executeQuery(sql);
 
+            //registro de la consulta de búsqueda
+            LOGGER.info("Consulta de búsqueda de empleado por teléfono");
+
             while (rs.next()) {
                 System.out.println("Nombre: " + rs.getString("nombre") + " Telefono: " + rs.getInt("telefono"));
             }
@@ -107,6 +142,10 @@ public class Conexionbd {
         } catch (SQLException e) {
             System.out.println("Error al buscar empleado");
             e.printStackTrace();
+
+            //registro del error en la consulta de búsqueda de empleado
+            LOGGER.log(Level.SEVERE, "Error al buscar empleado", e);
+
         }
     }
 
@@ -130,6 +169,9 @@ public class Conexionbd {
             String sql = "SELECT * FROM mascota WHERE nombre='" + nombre + "' AND raza='" + raza + "'";
             ResultSet rs = st.executeQuery(sql);
 
+            //registro de la consulta de búsqueda
+            LOGGER.info("Consulta de búsqueda de mascota por nombre y raza");
+
             while (rs.next()) {
                 System.out.println("Nombre: " + rs.getString("nombre") + " Raza: " + rs.getString("raza"));
             }
@@ -137,6 +179,10 @@ public class Conexionbd {
         } catch (SQLException e) {
             System.out.println("Error al buscar su mascota");
             e.printStackTrace();
+
+            //registro del error en la consulta de búsqueda de mascota
+            LOGGER.log(Level.SEVERE, "Error al buscar su mascota", e);
+
         }
     }
 
@@ -155,6 +201,10 @@ public class Conexionbd {
             String sql = "SELECT * FROM cita WHERE id='" + id + "'";
             ResultSet rs = st.executeQuery(sql);
 
+            //registro de la consulta de búsqueda
+            LOGGER.info("Consulta de búsqueda de cita por id");
+
+
             while (rs.next()) {
                 System.out.println("Id: " + rs.getInt("id") + " Fecha: " + rs.getString("fecha") + " Dueño_id: " + rs.getInt("dueño_id") + " Mascota_id: " + rs.getInt("mascota_id") + " Empleado_id: " + rs.getInt("empleado_id"));
             }
@@ -162,6 +212,9 @@ public class Conexionbd {
         } catch (SQLException e) {
             System.out.println("Error al buscar cita");
             e.printStackTrace();
+
+            //registro del error en la consulta de búsqueda de cita
+            LOGGER.log(Level.SEVERE, "Error al buscar cita", e);
         }
     }
 
@@ -189,15 +242,23 @@ public class Conexionbd {
 
             String sql1 = "INSERT INTO dueño (nombre, telefono) VALUES ('" + nombre + "','" + telefono + "')";
             st.executeUpdate(sql1, Statement.RETURN_GENERATED_KEYS);
+
             ResultSet rs = st.getGeneratedKeys();
+            // Obtener el id del dueño recién creado
             int idDueno = 0;
             if (rs.next()) {
                 idDueno = rs.getInt(1);
             }
+            // Registro de la creación del dueño
+            LOGGER.info("Dueño creado con id: " + idDueno);
+
 
             // Insertar datos de la mascota asociada al dueño
             String sql2 = "INSERT INTO mascota (nombre, raza, tipo_pelo, dueño_id) VALUES ('" + mascota + "', '" + raza + "','" + tipopelo + "'," + idDueno + ")";
             st.executeUpdate(sql2);
+
+            // Registro de la creación de la mascota
+            LOGGER.info("Mascota creada con nombre: " + mascota);
 
             System.out.println("Dueño y mascota creados exitosamente.");
 
@@ -205,6 +266,10 @@ public class Conexionbd {
         } catch (SQLException e) {
             System.out.println("Error al crear dueño");
             e.printStackTrace();
+
+            //registro del error en la consulta de creación de dueño
+            LOGGER.log(Level.SEVERE, "Error al crear dueño", e);
+
         }
     }
 
@@ -223,9 +288,11 @@ public class Conexionbd {
         try {
 
             Statement st = conexion.createStatement();
-
             String sql = "INSERT INTO empleado (nombre, telefono) VALUES ('" + nombre + "','" + telefono + "')";
             st.executeUpdate(sql);
+
+            //registro de la creación del empleado
+            LOGGER.info("Empleado creado con nombre: " + nombre);
 
             System.out.println("Empleado creado exitosamente.");
 
@@ -233,6 +300,9 @@ public class Conexionbd {
         } catch (SQLException e) {
             System.out.println("Error al crear empleado");
             e.printStackTrace();
+
+            //registro del error en la consulta de creación de empleado
+            LOGGER.log(Level.SEVERE, "Error al crear empleado", e);
         }
     }
 
@@ -254,6 +324,9 @@ public class Conexionbd {
             String sql1 = "SELECT * FROM dueño WHERE telefono = '" + telefono + "'";
             ResultSet rs = st.executeQuery(sql1);
 
+            // Registro de la consulta de búsqueda del dueño
+            LOGGER.info("Consulta de búsqueda de dueño por telefono");
+
             if (rs.next()) {
                 System.out.println("Dueño encontrado.");
                 System.out.println("Elige el campo a editar:");
@@ -271,6 +344,9 @@ public class Conexionbd {
                     String sql2 = "UPDATE dueño SET nombre = '" + nuevoNombre + "' WHERE telefono = '" + telefono + "'";
                     st.executeUpdate(sql2);
 
+                    // Registro de la actualización del nombre del dueño
+                    LOGGER.info("Actualización del nombre del dueño");
+
                     System.out.println("Nombre del dueño editado exitosamente.");
                 } else if (opcion == 2) {
                     System.out.println("Introduce el nuevo telefono del dueño");
@@ -279,6 +355,9 @@ public class Conexionbd {
                     // Actualizar el telefono del dueño
                     String sql3 = "UPDATE dueño SET telefono = '" + nuevoTelefono + "' WHERE telefono = '" + telefono + "'";
                     st.executeUpdate(sql3);
+
+                    // Registro de la actualización del telefono del dueño
+                    LOGGER.info("Actualización del telefono del dueño");
 
                     System.out.println("Telefono del dueño editado exitosamente.");
                 } else {
@@ -291,6 +370,9 @@ public class Conexionbd {
         } catch (SQLException e) {
             System.out.println("Error al editar dueño");
             e.printStackTrace();
+
+            // Registro del error en la consulta de edición del dueño
+            LOGGER.log(Level.SEVERE, "Error al editar dueño", e);
         }
     }
 
@@ -311,6 +393,9 @@ public class Conexionbd {
             String sql1 = "SELECT * FROM empleado WHERE telefono = '" + telefono + "'";
             ResultSet rs = st.executeQuery(sql1);
 
+            // Registro de la consulta de búsqueda del empleado
+            LOGGER.info("Consulta de búsqueda de empleado por telefono");
+
             if (rs.next()) {
                 System.out.println("Empleado encontrado.");
                 System.out.println("Elige el campo a editar:");
@@ -328,6 +413,9 @@ public class Conexionbd {
                     String sql2 = "UPDATE empleado SET nombre = '" + nuevoNombre + "' WHERE telefono = '" + telefono + "'";
                     st.executeUpdate(sql2);
 
+                    // Registro de la actualización del nombre del empleado
+                    LOGGER.info("Actualización del nombre del empleado");
+
                     System.out.println("Nombre del empleado editado exitosamente.");
                 } else if (opcion == 2) {
                     System.out.println("Introduce el nuevo telefono del empleado");
@@ -336,6 +424,9 @@ public class Conexionbd {
                     // Actualizar el telefono del empleado
                     String sql3 = "UPDATE empleado SET telefono = '" + nuevoTelefono + "' WHERE telefono = '" + telefono + "'";
                     st.executeUpdate(sql3);
+
+                    // Registro de la actualización del telefono del empleado
+                    LOGGER.info("Actualización del telefono del empleado");
 
                     System.out.println("Telefono del empleado editado exitosamente.");
                 } else {
@@ -348,6 +439,9 @@ public class Conexionbd {
         } catch (SQLException e) {
             System.out.println("Error al editar empleado");
             e.printStackTrace();
+
+            // Registro del error en la consulta de edición del empleado
+            LOGGER.log(Level.SEVERE, "Error al editar empleado", e);
         }
     }
 
@@ -369,10 +463,16 @@ public class Conexionbd {
         st.executeUpdate(sql);
         System.out.println("Empleado borrado exitosamente.");
 
+        // Registro de la eliminación del empleado
+        LOGGER.info("Eliminación del empleado");
+
         st.close();
     }catch (SQLException e) {
         System.out.println("Error al borrar empleado");
         e.printStackTrace();
+
+        // Registro del error en la consulta de eliminación del empleado
+        LOGGER.log(Level.SEVERE, "Error al borrar empleado", e);
     }
     }
 
@@ -391,12 +491,19 @@ public class Conexionbd {
 
             String sql = "DELETE FROM dueño WHERE telefono = '" + telefono + "'";
             st.executeUpdate(sql);
+
+            // Registro de la eliminación del dueño
+            LOGGER.info("Eliminación del dueño");
+
             System.out.println("Dueño borrado exitosamente.");
 
             st.close();
         }catch (SQLException e) {
             System.out.println("Error al borrar dueño");
             e.printStackTrace();
+
+            // Registro del error en la consulta de eliminación del dueño
+            LOGGER.log(Level.SEVERE, "Error al borrar dueño", e);
         }
     }
 
@@ -422,6 +529,10 @@ public class Conexionbd {
                     "INNER JOIN Mascota ON Cita.mascota_id = Mascota.id " +
                     "WHERE Cita.fecha = '" + fecha + "'";
             ResultSet rs = st.executeQuery(sql);
+
+            // Registro de la consulta de citas
+            LOGGER.info("Consulta de citas");
+
 
             System.out.println("Citas para la fecha " + fecha + ":");
             System.out.println("ID\tDueño\t\tEmpleado\tMascota");
@@ -493,6 +604,8 @@ public class Conexionbd {
         } catch (SQLException e) {
             System.out.println("Error al obtener las citas");
             e.printStackTrace();
+
+            // Registro del error en la consulta de citas
         }
 
     }
@@ -517,12 +630,18 @@ public class Conexionbd {
             String sql = "UPDATE Cita SET fecha = '" + nuevaFecha + "' WHERE id = " + idCita;
             st.executeUpdate(sql);
 
+            // Registro de la modificación de la cita
+            LOGGER.info("Modificación de la cita");
+
             System.out.println("Cita modificada exitosamente.");
 
             st.close();
         } catch (SQLException e) {
             System.out.println("Error al modificar la cita");
             e.printStackTrace();
+
+            // Registro del error en la modificación de la cita
+            LOGGER.info("error al modificar la cita");
         }
     }
 
@@ -542,6 +661,9 @@ public class Conexionbd {
                     " INNER JOIN Mascota m ON c.mascota_id = m.id" +
                     " INNER JOIN Empleado e ON c.empleado_id = e.id" +
                     " WHERE c.id = " + idCita;
+
+            // Ejemplo de consulta de los detalles de la cita
+            LOGGER.info("detalles de la cita");
 
             ResultSet rs = st.executeQuery(sql);
 
@@ -566,6 +688,9 @@ public class Conexionbd {
         } catch (SQLException e) {
             System.out.println("Error al obtener los detalles de la cita");
             e.printStackTrace();
+
+            // Registro del error en la consulta de los detalles de la cita
+            LOGGER.info("error al obtener los detalles de la cita");
         }
     }
 
@@ -583,12 +708,18 @@ public class Conexionbd {
             String sql = "DELETE FROM Cita WHERE id = " + idCita;
             st.executeUpdate(sql);
 
+            // Registro del borrado de la cita
+            LOGGER.info("borrado de la cita");
+
             System.out.println("Cita borrada exitosamente.");
 
             st.close();
         } catch (SQLException e) {
             System.out.println("Error al borrar la cita");
             e.printStackTrace();
+
+            // Registro del error en el borrado de la cita
+            LOGGER.info("error al borrar la cita");
         }
     }
 
@@ -608,6 +739,7 @@ public class Conexionbd {
             PreparedStatement stmtEmpleado = conexion.prepareStatement(sqlEmpleado);
             stmtEmpleado.setString(1, nombreEmpleado);
             ResultSet rsEmpleado = stmtEmpleado.executeQuery();
+
 
             int idEmpleado;
             if (rsEmpleado.next()) {
@@ -685,6 +817,5 @@ public class Conexionbd {
 
 }
     }
-
 
 }
--- src/log/Logs.java
+++ src/log/Logs.java
@@ -3,7 +3,7 @@ package log;
 import java.util.logging.Logger;
 
 public class Logs {
-    private static final Logger LOGGER = Logger.getLogger(Logs.class.getName());
+    public static final Logger LOGGER = Logger.getLogger(Logs.class.getName());
 
     public static void main(String[] args) {
         LOGGER.info("Este es un mensaje de información");
